a6228ae851e2fb324b4a9d8e44c0340a2e7c8d46
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-02-05 19:36:51 +0300

    feat[test]: implement note management system with authentication and routing

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note({ note, onDelete }) {
+    const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
+    const formattedTime = new Date(note.created_at).toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' });
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formattedDate} || {formattedTime}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default Note
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+function NotFound() {
+    return (
+    <>
+        <div className="relative">
+            <h1 className="text-9xl font-bold text-primary opacity-20"
+            aria-hidden="true">
+              404
+            </h1>
+          </div>
+        
+        <h2 className="flex justify-center mb-6 text-2xl font-medium text-onBackground ">Page Not Found</h2>
+        <p className="flex justify-center mb-6 text-onBackground/70">
+          The page you're looking for doesn't exist. Let's get you back!
+        </p>
+    </>
+    )
+}
+
+export default NotFound
